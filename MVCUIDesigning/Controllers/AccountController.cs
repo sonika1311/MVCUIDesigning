150d75091e3493affd920905ceffdc295980605b
sonika1311 <sonikakumaribom@gmail.com>
2024-06-30 19:01:33 +0530

    Designing views using HTML helper extension methods

---
+++ MVCUIDesigning/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCUIDesigning.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ViewResult Login()
+        {
+            return View();
+        }
+        public ViewResult Validate()
+        {
+            string txtName = Request["txtName"];
+            string txtPwd = Request["txtPwd"];
+            if (txtName == "admin" && txtPwd == "admin")
+            {
+                Session["User"] = txtName;
+                return View("Success");
+            }
+            else
+            {
+                ViewBag.Name = txtName;
+                return View("Failure");
+            }
+
+        }    
+    }
+}
