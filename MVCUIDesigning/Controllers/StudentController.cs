4a67071c45ab069e7b68a876fa0a9b9276d91fd0
sonika1311 <sonikakumaribom@gmail.com>
2024-06-30 19:28:05 +0530

    Creating UI using HTML helpers and reading form values (on server side ) using Model class

---
+++ MVCUIDesigning/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCUIDesigning.Models;
+
+namespace MVCUIDesigning.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ViewResult AddStudent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult AddStudent(Student student)
+        {
+            return View("DisplayStudent",student);
+        }
+
+    }
+}
